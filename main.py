a477255d102e7cca84427f86b3ae7e7b26cebf03
Zan <59426552+zZan54@users.noreply.github.com>
2022-02-22 00:57:51 +0100

    Added Type of generation
    Now you can chose what type of code generation do you want.

--- main.py
+++ main.py
@@ -8,14 +8,27 @@ class DiscordNitroGenerator:
  | \| (_) |_ _ _ ___ / __|___ _ _  ___ _ _ __ _| |_ ___ _ _ 
  | .` | |  _| '_/ _ \ (_ / -_) ' \/ -_) '_/ _` |  _/ _ \ '_|
  |_|\_|_|\__|_| \___/\___\___|_||_\___|_| \__,_|\__\___/_|  """
+        time.sleep(1)
         print(discordnitrogen)
         print(" by github.com/zZan54")
         print(" ")
-        gen.slowType("How many codes to generate: ", 0.05, newLine = False)
+        time.sleep(1.5)
+        gen.slowType(" How many codes to generate: ", 0.05, newLine = False)
         try:
             num = int(input(" "))
         except ValueError:
-            input("That isn't a number.\nPress enter to exit")
+            input(" That isn't a number.\n Press enter to exit")
+            exit()
+        print(" ")
+        print(" 1 - With https://")
+        print(" 2 - Without https://")
+        print(" 3 - Only the code")
+        print(" ")
+        gen.slowType(" Type of code generation: ", 0.05, newLine = False)
+        try:
+            type = int(input(" "))
+        except ValueError:
+            input(" That isn't a number.\n Press enter to exit")
             exit()
         chars = []
         chars[:0] = string.ascii_letters + string.digits
@@ -23,8 +36,16 @@ class DiscordNitroGenerator:
         for s in c:
             try:
                 code = ''.join(x for x in s)
-                url = f"https://discord.gift/{code}"
-
+                
+                url1 = f" https://discord.gift/{code}"
+                url2 = f" discord.gift/{code}"
+                url3 = f" {code}"
+                if type == 1:
+                    url = url1
+                elif type == 2:
+                    url = url2
+                elif type == 3:
+                    url = url3
                 print(url)
             except KeyboardInterrupt:
                break
